45f6a94086dc957858a8c966e9c4c6b12a4f769d
Huy.PV <nhuthuy212507@gmail.com>
2018-12-04 23:36:13 +0700

    fix a bug

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -21,7 +21,7 @@ var myReducer = (state = initialState, action) => {
       var task = {
         id: action.task.id,
         name: action.task.name,
-        status: action.task.status === 'true' ? true : false
+        status: (action.task.status === 'true' || action.task.status === true) ? true : false
       };
       if (!task.id) {
         task.id = guidGenerator();
